bc4b784a1d8998318655185f5a9997ba28c66a40
KaiyangNi <KaiyangNi@users.noreply.github.com>
2017-12-09 02:03:20 +0800

    Update hostloc.py

--- hostloc.py
+++ hostloc.py
@@ -10,10 +10,8 @@ appid2 = os.getenv('appid2')
 
 def hostloc():
     headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 'Referer':'http://www.hostloc.com'}
-    session = requests.Session()
-    session.headers.update(headers)
-    
     s = requests.Session()
+    s.headers.update(headers)
     s.post("http://www.hostloc.com/member.php", {'username':appid1, 'password':appid2})
     urls = ["http://www.hostloc.com/space-uid-{}.html".format(str(random.randrange(10000, 25000))) for i in range(12)]
     for i in urls:
